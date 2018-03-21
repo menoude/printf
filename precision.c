d90ccace35f5269ed7b03431a17a40c0a1e031f9
Mennad YAMI <meyami@e2r4p19.42.fr>
2018-03-21 21:07:57 +0100

    indentation

--- precision.c
+++ precision.c
@@ -1,6 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   precision.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: meyami <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/03/21 21:06:01 by meyami            #+#    #+#             */
+/*   Updated: 2018/03/21 21:06:03 by meyami           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_printf.h"
 
-static void precision_handle(t_env *e, char *precision)
+static void    precision_handle(t_env *e, char *precision)
 {
   int i;
   int n;
@@ -18,7 +30,7 @@ static void precision_handle(t_env *e, char *precision)
     e->precision_zero = 1;
 }
 
-int precision_find(t_env *e, char *precision)
+int            precision_find(t_env *e, char *precision)
 {
   if (ft_substr(".", precision[0]) && !e->length && !e->type)
   {
